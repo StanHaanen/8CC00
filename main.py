2327fb80b3be6425b7296d568fdf2b8c5a92a3f4
StanHaanen <s.haanen@live.nl>
2023-05-03 14:03:19 +0200

    "Last updates"

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ def read_intervals(filepath):
         intervals = [line.strip() for line in f.readlines()]
     return [tuple(int(bound.strip('[], ')) for bound in interval.split(',')) for interval in intervals]
 
-#print(read_intervals('set1.txt'))
-#print(read_intervals('set2.txt'))
-
 def similarity_between_lines(line1, line2):
     intervals1 = [tuple(line1[i:i+2]) for i in range(0, len(line1), 2)]
     intervals2 = [tuple(line2[i:i+2]) for i in range(0, len(line2), 2)]
@@ -25,8 +22,22 @@ def symmetric_similarity(set_1, set_2):
     sim21 = similarity_between_lines(set_2, set_1)
     return (sim12 + sim21) / 2
 
+def similarity(set_1, set_2, outfile):
+    lines_set_1 = read_intervals(set_1)
+    lines_set_2 = read_intervals(set_2)
 
-print(symmetric_similarity((2, 5, 11, 17, 22, 37), (3, 8, 18, 20, 24, 26, 29, 33)))
-#print(symmetric_similarity((110, 117, 255, 263), (117, 120, 240, 256, 259, 307)))
-#print(symmetric_similarity((44, 66, 87, 104, 188, 204), (20, 44, 71, 75, 180, 192, 303, 315)))
-#print(symmetric_similarity((2, 33, 47, 56, 90, 99, 301, 312, 554, 707), (7, 35, 45, 58, 101, 119, 1043, 1352)))
+    num_lines_1 = len(lines_set_1)
+    num_lines_2 = len(lines_set_2)
+
+    if num_lines_1 != num_lines_2:
+        print("Warning: Sets do not contain same amount of lists.")
+
+    similarities = []
+    for i in range(num_lines_1):
+        sim = symmetric_similarity(lines_set_1[i], lines_set_2[i])
+        similarities.append(sim)
+
+    avg_sim = sum(similarities) / (num_lines_1)
+
+    with open(outfile, 'w') as f:
+        f.write('{:.2f}'.format(avg_sim))

b4eb674da0b1c0c63b1493545fd6ecf8f57ea5a0
StanHaanen <s.haanen@live.nl>
2023-05-03 14:54:52 +0200

    Working code, V1

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def read_intervals(filepath):
 def similarity_between_lines(line1, line2):
     intervals1 = [tuple(line1[i:i+2]) for i in range(0, len(line1), 2)]
     intervals2 = [tuple(line2[i:i+2]) for i in range(0, len(line2), 2)]
+    
     count = 0
+    j = 0
     for interval1 in intervals1:
-        for interval2 in intervals2:
-            if max(interval1[0], interval2[0]) <= min(interval1[1], interval2[1]):
-                count += 1
-                break
+        while j < len(intervals2) and intervals2[j][1] < interval1[0]:
+            j += 1
+        if j == len(intervals2):
+            break
+        if interval1[1] >= intervals2[j][0]:
+            count += 1
     similarity = count / max(len(intervals1), len(intervals2))
     return similarity
 
@@ -23,8 +27,11 @@ def symmetric_similarity(set_1, set_2):
     return (sim12 + sim21) / 2
 
 def similarity(set_1, set_2, outfile):
-    lines_set_1 = read_intervals(set_1)
-    lines_set_2 = read_intervals(set_2)
+    with open(set_1, 'r') as f:
+        lines_set_1 = [tuple(int(bound.strip('[], ')) for bound in line.strip().split(',')) for line in f.readlines()]
+
+    with open(set_2, 'r') as f:
+        lines_set_2 = [tuple(int(bound.strip('[], ')) for bound in line.strip().split(',')) for line in f.readlines()]
 
     num_lines_1 = len(lines_set_1)
     num_lines_2 = len(lines_set_2)
@@ -34,10 +41,13 @@ def similarity(set_1, set_2, outfile):
 
     similarities = []
     for i in range(num_lines_1):
-        sim = symmetric_similarity(lines_set_1[i], lines_set_2[i])
+        sim12 = similarity_between_lines(lines_set_1[i], lines_set_2[i])
+        sim21 = similarity_between_lines(lines_set_2[i], lines_set_1[i])
+        sim = (sim12 + sim21) / 2
         similarities.append(sim)
 
-    avg_sim = sum(similarities) / (num_lines_1)
+    avg_sim = sum(similarities) / num_lines_1
 
     with open(outfile, 'w') as f:
         f.write('{:.2f}'.format(avg_sim))
+
